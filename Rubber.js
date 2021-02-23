12d8125520574bd3adc18f806d2a42468c6739aa
HarshikaJain <76556299+HarshikaJain@users.noreply.github.com>
2021-02-23 19:22:40 +0530

    Add files via upload

--- Rubber.js
+++ Rubber.js
@@ -8,21 +8,21 @@ class Rubber{
 		}
 		this.x=x;
 		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.r=r;
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
 		World.add(world, this.body);
 
 	}
 	display()
 	{
 			var rubberpos=this.body.position;		
-			push()
+			push();
 			translate(rubberpos.x, rubberpos.y);
-			rectMode(CENTER)
+			rectMode(CENTER);
 			strokeWeight(4);
 			stroke("black");
 			fill("darkblue");
 			ellipse(0,0,this.r, this.r);
-			pop()
+			pop();
 	}
 }
--- Sand.js
+++ Sand.js
@@ -8,21 +8,21 @@ class Sand{
 		}
 		this.x=x;
 		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.r=r;
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
 		World.add(world, this.body);
 
 	}
 	display()
 	{
 			var sandpos=this.body.position;		
-			push()
+			push();
 			translate(sandpos.x, sandpos.y);
-			rectMode(CENTER)
+			rectMode(CENTER);
 			strokeWeight(4);
 			stroke("black");
 			fill("red");
 			ellipse(0,0,this.r, this.r);
-			pop()
+			pop();
 	}
 }
